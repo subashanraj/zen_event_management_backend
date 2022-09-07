91f0be8308be94a67c1bf1ec7b2e17d6c1f4c3b8
subashanraj <104359010+subashanraj@users.noreply.github.com>
2022-09-07 14:50:31 +0530

    Add files via upload

---
+++ routes/index.js
@@ -0,0 +1,86 @@
+﻿import express from "express";
+import { MongoClient } from "mongodb";
+import dotenv from "dotenv";
+import cors from "cors";
+import { response } from "express";
+import bcrypt from "bcrypt";
+
+// dotenv.config()
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 5000;
+
+const MONGO_URL = process.env.MONGODB_URL;
+
+async function createConnection() {
+    const client = new MongoClient(MONGO_URL);
+    await client.connect();
+    console.log("MongoDB is connected to server 😎😍");
+    return client;
+}
+const client = await createConnection();
+
+async function createPassword(password) {
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+    return hashPassword;
+}
+
+app.get("/", (request, response) => {
+    response.send("Welcome to zen Login");
+});
+
+////  SignUp data for Login registration.
+
+app.post("/SignUP", async (request, response) => {
+    const { name, email, contact, password, permission, id, surName } =
+        request.body;
+
+    const hashPassword = await createPassword(password);
+    console.log(hashPassword);
+    const newUser = {
+        name: name,
+        surName: surName,
+        password: hashPassword,
+        email: email,
+        contact: contact,
+        permission: permission,
+        id: id,
+    };
+
+    const adminSignUp = await client
+        .db("zen_task")
+        .collection("signUp")
+        .insertOne(newUser);
+
+    response.send(adminSignUp);
+});
+
+////  SignIn data for  Login
+
+app.post("signin", async (request, response) => {
+    const { username, password } = request.body;
+
+    const signIn = await client
+        .db("zen_task")
+        .collection("signUp")
+        .findOne(username);
+    if (!signIn) {
+        response.status(401).send({ message: "Invalid credintials" });
+        console.log({ message: "Invalid credintials" });
+    } else {
+        const storedPassword = signIn.password;
+        const isPasswordMatch = await bcrypt.compare(password, storedPassword);
+        console.log("isPasswordMatch", isPasswordMatch);  //// Returns a boolean value
+        if (!isPasswordMatch) {
+            response.status(401).send({ message: "invalid credentials" });
+        } else {
+            response.send("successful login");
+        }
+    }
+});
+
+app.listen(PORT, () => console.log(`app connected to port${PORT} 😎😊`));
